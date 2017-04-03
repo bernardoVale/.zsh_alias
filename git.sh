57308dc661b81c0bb57ff1f56277ff2f01b1bf09
Bernardo Vale <bernardosilveiravale@gmail.com>
2017-04-03 12:07:48 -0300

    init

---
+++ git.sh
@@ -0,0 +1,9 @@
+gmail(){
+  if [ $1 = 'avenuecode' ]; then
+    git config --global user.email bvale@avenuecode.com
+  elif [ $1 = 'wsgc' ]; then
+    git config --global user.email bvale@wsgc.com
+  elif [ $1 = 'gmail' ]; then
+    git config --global user.email bernardosilveiravale@gmail.com
+  fi
+}
